5ad07dd9dfe0b64471797644211cf04830fbaade
vishnuoum <vishnuoumvishnuoum123@gmail.com>
2022-05-12 16:59:28 +0530

    Added speech to text and chat text box

--- lib/pages/signChat.dart
+++ lib/pages/signChat.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import 'package:text_to_speech/text_to_speech.dart';
 
 class SignChat extends StatefulWidget {
@@ -10,15 +13,225 @@ class SignChat extends StatefulWidget {
 
 class _SignChatState extends State<SignChat> {
 
-  List<Map> chat = [
-    {"person":"you","content":"Hello"},
-    {"person":"other","content":"Hello"},
-  ];
+  List<Map> chat = [];
+
+  SpeechToText _speechToText = SpeechToText();
+  bool listening = false, chatting = false, _speechEnabled = false;
+  String words="";
+
 
   TextToSpeech tts = TextToSpeech();
 
-  Color? chatColor = Colors.blue[100],buttonColor = Colors.blue;
+  Color? chatColor = Colors.blue[100],buttonColor = Colors.blue,chatBoxColor = Colors.blue[100];
   static double iconSize = 30, splashRadius = 70;
+  TextEditingController chatController = TextEditingController(text: "");
+
+  @override
+  void initState() {
+    _initSpeech();
+    super.initState();
+  }
+
+  // Init speech engine
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize(
+      onStatus: statusListener,
+      onError: errorListener
+    );
+    setState(() {});
+  }
+
+  // Start listen
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult,cancelOnError: true,listenFor: Duration(minutes: 1));
+    words = "";
+  }
+
+  // Stop listen
+  void _stopListening() async {
+    await _speechToText.stop();
+    if(words.length!=0)
+      setState(() {
+        chat.add({"person":"you","content":words});
+      });
+  }
+
+  // Speech result
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    print(result);
+    setState(() {
+      words = "$words ${result.recognizedWords}";
+    });
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    print('Received error status: $error');
+  }
+
+  void statusListener(String status) {
+    print(status);
+    print(listening);
+    print('Received listener status: $status, listening: ${_speechToText.isListening}');
+    if((!_speechToText.isListening || status == "done") && listening){
+      setState(() {
+        listening = false;
+      });
+      if(words.length!=0)
+        setState(() {
+          chat.add({"person":"you","content":words});
+        });
+    }
+  }
+
+
+
+  Widget bottomNav(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Material(
+          child: InkWell(
+            radius: 50,
+            borderRadius: BorderRadius.circular(splashRadius),
+            onTap: () {},
+            onLongPress: () {
+              tts.speak("Convert sign to text");
+            },
+            child: Ink(
+              padding: EdgeInsets.all(10),
+              child: Icon(Icons.accessibility,color: buttonColor,size: iconSize,),
+            ),
+          ),
+        ),
+        Material(
+          child: InkWell(
+            radius: 50,
+            borderRadius: BorderRadius.circular(splashRadius),
+            onTap: _speechEnabled?() {
+              print("tapped");
+              setState(() {
+                listening = true;
+                _startListening();
+              });
+            }:null,
+            onLongPress: () {
+              tts.speak("Convert speech to text");
+            },
+            child: Ink(
+              padding: EdgeInsets.all(10),
+              child: Icon(Icons.mic,color: buttonColor,size: iconSize,),
+            ),
+          ),
+        ),
+        Material(
+          child: InkWell(
+            radius: 50,
+            borderRadius: BorderRadius.circular(splashRadius),
+            onTap: () {
+              print("tapped");
+              setState(() {
+                chatting = true;
+              });
+            },
+            onLongPress: () {
+              tts.speak("Type in your message");
+            },
+            child: Ink(
+              padding: EdgeInsets.all(10),
+              child: Icon(Icons.chat,color: buttonColor,size: iconSize,),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget audioNav(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          flex: 5,
+          child: SingleChildScrollView(
+            child: Center(child: Text(words)),
+          )
+        ),
+        Expanded(
+          child: CircleAvatar(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.red,
+            child: IconButton(
+              icon: Icon(Icons.record_voice_over),
+              onPressed: (){
+                setState(() {
+                  listening = false;
+                });
+                _stopListening();
+              },
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget chatNav(){
+    return Container(
+      padding: const EdgeInsets.all(5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+              flex: 7,
+              child: Container(
+                margin: EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  color: chatBoxColor,
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  autofocus: true,
+                  controller: chatController,
+                  scrollPhysics: BouncingScrollPhysics(),
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: "Type in message",
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (val){
+                    setState(() {});
+                  },
+                ),
+              )
+          ),
+          Expanded(
+            child: Tooltip(
+              message: chatController.text.length!=0?"Message":"Close editor",
+              verticalOffset: 0,
+              child: IconButton(
+                color: buttonColor,
+                splashRadius: 25,
+                icon: Icon(chatController.text.length!=0?Icons.send:Icons.close),
+                onPressed: chatController.text.length!=0?(){
+                  setState(() {
+                    chat.add({"person":"you","content":chatController.text});
+                  });
+                  chatController.text="";
+                }:(){
+                  setState(() {
+                    chatting = false;
+                  });
+                  chatController.clear();
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   
   @override
   Widget build(BuildContext context) {
@@ -30,7 +243,7 @@ class _SignChatState extends State<SignChat> {
         child: Column(
           children: [
             Expanded(
-                flex: 20,
+                flex: 8,
                 child: ListView.builder(
                   reverse: true,
                   // controller: scrollController,
@@ -38,72 +251,35 @@ class _SignChatState extends State<SignChat> {
                   itemCount: chat.length,
                   itemBuilder: (context,index){
                     index=chat.length - 1 - index;
-                    return Align(
-                      alignment: chat[index]["person"]!="you"?Alignment.centerLeft:Alignment.centerRight,
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: chatColor,
+                    return GestureDetector(
+                      onLongPress: (){
+                        tts.speak(chat[index]["content"]);
+                      },
+                      child: Align(
+                        alignment: chat[index]["person"]!="you"?Alignment.centerLeft:Alignment.centerRight,
+                        child: Container(
+                          margin: EdgeInsets.only(top: 12),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: chatColor,
+                          ),
+                          constraints: BoxConstraints(
+                              maxWidth: MediaQuery.of(context).size.width*0.65
+                          ),
+                          child: Padding(padding: chat[index]["person"]=="you"?EdgeInsets.only(right: 5):EdgeInsets.only(left: 5),child: Text(chat[index]["content"],style: TextStyle(fontSize: 16),),),
                         ),
-                        constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width*0.65
-                        ),
-                        child: Padding(padding: chat[index]["person"]=="you"?EdgeInsets.only(right: 5):EdgeInsets.only(left: 5),child: Text(chat[index]["content"],style: TextStyle(fontSize: 16),),),
                       ),
                     );
                   },
                 )
             ),
-            Expanded(
-              flex: 2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Material(
-                    child: InkWell(
-                      radius: 50,
-                      borderRadius: BorderRadius.circular(splashRadius),
-                      onTap: () {},
-                      onLongPress: () {
-                        tts.speak("Convert speech to text");
-                      },
-                      child: Ink(
-                        padding: EdgeInsets.all(10),
-                        child: Icon(Icons.mic,color: buttonColor,size: iconSize,),
-                      ),
-                    ),
-                  ),
-                  Material(
-                    child: InkWell(
-                      radius: 50,
-                      borderRadius: BorderRadius.circular(splashRadius),
-                      onTap: () {},
-                      onLongPress: () {
-                        tts.speak("Convert sign to text");
-                      },
-                      child: Ink(
-                        padding: EdgeInsets.all(10),
-                        child: Icon(Icons.accessibility,color: buttonColor,size: iconSize,),
-                      ),
-                    ),
-                  ),
-                  Material(
-                    child: InkWell(
-                      radius: 50,
-                      borderRadius: BorderRadius.circular(splashRadius),
-                      onTap: () {},
-                      onLongPress: () {
-                        tts.speak("Type in your message");
-                      },
-                      child: Ink(
-                        padding: EdgeInsets.all(10),
-                        child: Icon(Icons.chat,color: buttonColor,size: iconSize,),
-                      ),
-                    ),
-                  ),
-                ],
+            Container(
+              constraints: BoxConstraints(
+                maxHeight: 100,
+                minHeight: 65
               ),
+              child: listening?audioNav():chatting?chatNav():bottomNav(),
             )
           ],
         ),
