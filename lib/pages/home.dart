1d8ef60a46a427d7889106fea8c94f6306064860
vishnuoum <vishnuoumvishnuoum123@gmail.com>
2022-05-15 17:10:47 +0530

    Added bottom nav bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,32 +8,88 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int selectedIndex = 0;
+
+  bool recording = false;
+
+  Widget homeScreen(){
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        ElevatedButton(onPressed: (){
+          Navigator.pushNamed(context, "/signChat");
+        }, child: Text("Sign Chat")),
+        // ElevatedButton(onPressed: (){
+        //   Navigator.pushNamed(context, "/audioNav");
+        // }, child: Text("Audio Nav")),
+        ElevatedButton(onPressed: (){
+          Navigator.pushNamed(context, "/audioNavCustom");
+        }, child: Text("Audio Nav Custom")),
+        ElevatedButton(onPressed: (){}, child: Text("Voice")),
+        ElevatedButton(onPressed: (){}, child: Text("Learn")),
+      ],
+    );
+  }
+  Widget settingsScreen(){
+    return ListView(
+      children: [
+        ListTile(title: Text("Settings"),onTap: (){},)
+      ],
+    );
+  }
+
+  void onTapItem(index){
+    if(index==1)
+      return;
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    Color navbarSelect = Colors.blue;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Swaram"),
+        title: Text("AIeSSA"),
       ),
-      body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, "/signChat");
-          }, child: Text("Sign Chat")),
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, "/audioNav");
-          }, child: Text("Audio Nav")),
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, "/audioNavCustom");
-          }, child: Text("Audio Nav Custom")),
-          ElevatedButton(onPressed: (){}, child: Text("Voice Assign")),
-          ElevatedButton(onPressed: (){}, child: Text("Learn")),
-        ],
-      )
+      body: <Widget>[homeScreen(),SizedBox(),settingsScreen()][selectedIndex],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: recording?Colors.red:Colors.blue,
+        onPressed: (){
+          setState(() {
+            recording = ! recording;
+          });
+        },
+        child: Icon(Icons.mic),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent
+        ),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.transparent,),label: "",tooltip: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Settings"),
+          ],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: selectedIndex,
+          elevation: 5,
+          onTap: onTapItem,
+        ),
+      ),
     );
   }
 }
